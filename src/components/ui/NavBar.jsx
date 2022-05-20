f5e351d99e91f4cf77a0e35d022a13d39ad14c76
lascano.valencia.fidel@gmail.com <Df3a0b951>
2022-05-19 23:02:11 -0500

    Add: navbar

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import {Link, NavLink} from "react-router-dom";
+import icon from "../../assets/img/superhero1.png"
 
 export const NavBar = () => {
   return (
-    <div>
-        <nav>
-            <ul>
-                <li>
-                    
-                </li>
-            </ul>
-        </nav>
-    </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+              <Link className="navbar-brand" to="/"><img src={icon} alt="icon"/></Link>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div className="navbar-nav">
+                      <NavLink className="nav-link" to="login">Login</NavLink>
+                      <NavLink className="nav-link" to="search">Search</NavLink>
+                      <NavLink className="nav-link" to="dc">Dc</NavLink>
+                      <NavLink className="nav-link" to="marvel">Marvel</NavLink>
+                  </div>
+              </div>
+          </div>
+      </nav>
   )
 }
