eece2503a361403a9189c712ae226e665a23e6c4
lascano.valencia.fidel@gmail.com <Df3a0b951>
2022-06-15 23:20:02 -0500

    create search page

--- src/components/search/SearchScreen.jsx
+++ src/components/search/SearchScreen.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import {useForm} from "../../hooks/useForm";
 
 export const SearchScreen = () => {
-    const [formData, handlerInputChange, restForm] = useForm({
+    const [{searchText}, handlerInputChange, restForm] = useForm({
         searchText: ""
     });
-
-    let {searchText} = formData;
 
     const handlerSubmit = event => {
         event.preventDefault();
@@ -20,22 +18,30 @@ export const SearchScreen = () => {
     }
 
     return (
-        <div className={"container"}>
+        <div className={"container row"}>
+            <div className={"col-5 m-1"}>
+
             <h1>Search Super Hero</h1>
             <form action={handlerSubmit} className={"form"}>
-                <div className={"col-5"} style={{paddingBottom:"1rem"}}>
+                <div className={""} style={{paddingBottom:"1rem"}}>
                     <input type="text"
-                           value={searchText.searchText}
+                           value={searchText}
                            onChange={handlerInputChange}
                            name={"searchText"}
                            className={"form-control"}
                     />
                 </div>
-                <div className={"col-5"}>
-
+                <div className={""}>
                     <button className={"btn btn-outline-primary"} onClick={handlerClickSearch}>Search</button>
                 </div>
             </form>
+            </div>
+            <div className={"col-6 m-1"}>
+                <h1>Result</h1>
+                <hr/>
+                <div className={"alert alert-primary"}>Search a hero</div>
+                <div className={"alert alert-danger"}>Hero not find</div>
+            </div>
         </div>
     )
 }
